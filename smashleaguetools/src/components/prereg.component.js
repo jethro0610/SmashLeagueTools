77fe183de3f62fb3872bcc4b99fed9a37c19a446
JetDesktop <jethro0610@outlook.com>
2021-06-17 19:05:11 -0700

    Restyle registration

--- smashleaguetools/src/components/prereg.component.js
+++ smashleaguetools/src/components/prereg.component.js
@@ -5,11 +5,11 @@ const PreReg = () => {
         <div className='container-fluid prereg'>
             <div className ='row align-items-center h-100'>
                 <div className ='col text-center text-dark'>
-                    <div>The Return</div>
-                    <div>6.19.2021</div>
+                    The Return <br/>
+                    6.19.2021   <br/>
                     <a rel='noopener noreferrer' target='_blank' 
                     href={process.env.REACT_APP_BACKEND_ORIGIN + '/tournament/signup'} 
-                    className='bg-dark text-light rounded-pill register'>Register on Smash.GG</a>
+                    className='rounded-pill register'>Register on Smash.GG</a>
                 </div>
             </div>
         </div>
