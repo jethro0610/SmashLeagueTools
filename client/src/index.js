f1ff29496a691e37a95648065c79b8d17f07b8e5
JetDesktop <jethro0610@outlook.com>
2021-06-18 14:14:55 -0700

    Remove unecessary logs

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store/store';
 import './socketmanager'
 require('dotenv').config({path:'../.env.' + process.env.NODE_ENV});
-console.log(process.env);
+
 ReactDOM.render(
   <Provider store = {store}>
     <React.StrictMode>
